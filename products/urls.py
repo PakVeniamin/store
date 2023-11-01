a35080efa19699dd7c6fdf692cd18a612e86974b
pakdevman <veniaminpak.nsu@mail.ru>
2023-11-01 20:53:00 +0700

    FBV to CBV Basket add and remove

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 
-from products.views import ProductsListView, basket_add, basket_remove
+from products.views import ProductsListView, BasketAddView, BasketRemoveView
 
 app_name = 'products'
 
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', ProductsListView.as_view(), name='product'),
     path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),
     path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
-    path('basket/add/<int:product_id>/', basket_add, name='basket_add'),
-    path('basket/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
+    path('basket/add/<int:product_id>/', BasketAddView.as_view(), name='basket_add'),
+    path('basket/remove/<int:basket_id>/', BasketRemoveView.as_view(), name='basket_remove'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from products.models import Products, Categories, Basket
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, View
 from django.views.generic.list import ListView
+
 
 # Create your views here.
 
@@ -33,37 +34,25 @@ class ProductsListView(ListView):
         return context
 
 
-# def products(request, category_id=None, page_numbers=1):
-#     products = Products.objects.filter(category_id=category_id) if category_id else Products.objects.all()
-#     per_page = 2
-#     paginator = Paginator(products, per_page)
-#     products_paginator = paginator.page(page_numbers)
-#
-#     context = {
-#         'title': 'Store - Каталог',
-#         'products': products_paginator,
-#         'category': Categories.objects.all(),
-#         'paginator': paginator
-#     }
-#     return render(request, 'products/products.html', context)
+class BasketAddView(View):
+    model = Basket
+    template_name = 'products/products.html'
+
+    def post(self, request, product_id):
+        product = Products.objects.get(id=product_id)
+        baskets = Basket.objects.filter(user=request.user, product=product)
+        if not baskets.exists():
+            Basket.objects.create(user=request.user, product=product)
+        else:
+            basket = baskets.first()
+            basket.quantity += 1
+            basket.save()
+
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
-@login_required
-def basket_add(request, product_id):
-    product = Products.objects.get(id=product_id)
-    baskets = Basket.objects.filter(user=request.user, product=product)
-    if not baskets.exists():
-        Basket.objects.create(user=request.user, product=product)
-    else:
-        basket = baskets.first()
-        basket.quantity += 1
-        basket.save()
-
-    return HttpResponseRedirect(request.META['HTTP_REFERER'])
-
-
-@login_required
-def basket_remove(request, basket_id):
-    basket = Basket.objects.get(id=basket_id)
-    basket.delete()
-    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+class BasketRemoveView(View):
+    def post(self, request, basket_id):
+        basket = Basket.objects.get(id=basket_id)
+        basket.delete()
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
